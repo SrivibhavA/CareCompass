1e82018a91e391b20b223c871000c611fafffe09
TheRealDhruv <DhruvSiri04@gmail.com>
2025-01-01 13:56:37 -0600

    Added Login functionality and fixed the Previous Entry UI

--- classes/journal.py
+++ classes/journal.py
@@ -5,7 +5,7 @@ class Journal:
         self.date = date
     
     def save(self):
-        with open("output.txt", "a") as file:
+        with open("data/output.txt", "a") as file:
             # Write the variables to the file
             file.write(f"Text: {self.text};")
             file.write(f"Feeling Score: {self.feeling_score};")
